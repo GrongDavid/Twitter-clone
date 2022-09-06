2311c1d0d43fc0dff033dc3f0771e62eca450e61
David <davidgrong778@gmail.com>
2022-09-06 03:14:44 -0500

    view test for messages

--- test_message_views.py
+++ test_message_views.py
@@ -2,13 +2,13 @@
 
 # run these tests like:
 #
-#    FLASK_ENV=production python -m unittest test_message_views.py
+#    FLASK_ENV=production python -message unittest test_message_views.py
 
 
 import os
 from unittest import TestCase
 
-from models import db, connect_db, Message, User
+from models import db, connect_db, Message, diff_user
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -20,7 +20,7 @@ os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
 
 # Now we can import app
 
-from app import app, CURR_USER_KEY
+from app import app, CURR_diff_user_KEY
 
 # Create our tables (we do this here, so we only create the tables
 # once for all tests --- in each test, we'll delete the data
@@ -39,15 +39,17 @@ class MessageViewTestCase(TestCase):
     def setUp(self):
         """Create test client, add sample data."""
 
-        User.query.delete()
-        Message.query.delete()
+        db.drop_all()
+        db.create_all()
 
         self.client = app.test_client()
 
-        self.testuser = User.signup(username="testuser",
-                                    email="test@test.com",
-                                    password="testuser",
+        self.diff_user = diff_user.signup(diff_username="diff_user",
+                                    email="testemail@gmail.com",
+                                    password="abc123",
                                     image_url=None)
+        self.diff_user_id = 8989
+        self.diff_user.id = self.diff_user_id
 
         db.session.commit()
 
@@ -59,15 +61,98 @@ class MessageViewTestCase(TestCase):
 
         with self.client as c:
             with c.session_transaction() as sess:
-                sess[CURR_USER_KEY] = self.testuser.id
+                sess[CURR_diff_user_KEY] = self.diff_user.id
 
             # Now, that session setting is saved, so we can have
             # the rest of ours test
 
-            resp = c.post("/messages/new", data={"text": "Hello"})
+            response = c.post("/messages/new", data={"text": "testText"})
 
             # Make sure it redirects
-            self.assertEqual(resp.status_code, 302)
+            self.assertEqual(response.status_code, 302)
 
-            msg = Message.query.one()
-            self.assertEqual(msg.text, "Hello")
+            message = Message.query.one()
+            self.assertEqual(message.text, "testText")
+
+    def test_add(self):
+        with self.client as c:
+            response = c.post("/messages/new", data={"text": "testText"}, follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn("Access unauthorized", str(response.data))
+
+    def test_invalid_diff_user(self):
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_diff_user_KEY] = 000
+
+            response = c.post("/messages/new", data={"text": "testText"}, follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn("Access unauthorized", str(response.data))
+    
+    def test_message(self):
+
+        message = Message(id=1, text="random message", diff_user_id=self.diff_user_id)
+        
+        db.session.add(message)
+        db.session.commit()
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_diff_user_KEY] = self.diff_user.id
+            
+            message = Message.query.get(1)
+
+            response = c.get(f'/messages/{message.id}')
+
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(message.text, str(response.data))
+
+    def test_invalid_message(self):
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_diff_user_KEY] = self.diff_user.id
+            
+            response = c.get('/messages/0')
+            self.assertEqual(response.status_code, 404)
+
+    def test_message_delete(self):
+
+        message = Message(id=1, text="a test message", diff_user_id=self.diff_user_id)
+        db.session.add(message)
+        db.session.commit()
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_diff_user_KEY] = self.diff_user.id
+
+            response = c.post("/messages/1/delete", follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
+
+            message = Message.query.get(1)
+            self.assertIsNone(message)
+
+    def test_non_owner_message_delete(self):
+        diff_user = diff_user.signup(diff_username="diff",
+                        email="bademail@gmail.com",
+                        password="abc123",
+                        image_url=None)
+        diff_user.id = 2
+
+        #Message is owned by diff_user
+        message = Message(id=1, text="more random messages", diff_user_id=self.diff_user_id
+        )
+        db.session.add(diff_user)
+        db.session.add(message)
+        db.session.commit()
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess['CURR_diff_user_KEY'] = 2
+
+            response = c.post("/messages/1/delete", follow_redirects=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn("Access unauthorized", str(response.data))
+
+            message = Message.query.get(1)
+            self.assertIsNotNone(message)
+
