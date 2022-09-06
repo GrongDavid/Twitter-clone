9baa4994fe3c240b51f51d31374e27c59857876d
David <davidgrong778@gmail.com>
2022-09-06 02:08:07 -0500

    added initial testing

--- test_user_model.py
+++ test_user_model.py
@@ -39,7 +39,27 @@ class UserModelTestCase(TestCase):
         Message.query.delete()
         Follows.query.delete()
 
+        user1 = User.signup('user1', 'testemail1@gmail.com', 'abc123', None)
+        user1.id = 1
+
+        user2 = User.signup('user2', 'testemail2@gmail.com', 'abc123', None)
+        user2.id = 2
+
+        db.session.commit()
+
+        u1 = User.query.get(1)
+        u2 = User.query.get(2)
+
+        self.u1 = u1
+
+        self.u2 = u2
+
         self.client = app.test_client()
+    
+    def tearDown(self):
+        down = super().tearDown()
+        db.session.rollback()
+        return down
 
     def test_user_model(self):
         """Does basic model work?"""
@@ -56,3 +76,59 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+
+    def test_follows(self):
+        self.u1.following.append(self.u2)
+        db.session.commit()
+
+        # User 2 should be following none and have 1 follower
+        self.assertEqual(len(self.u2.following), 0)
+        self.assertEqual(len(self.u2.followers), 1)
+
+        # User 1 should have no followers but be following 1
+        self.assertEqual(len(self.u1.followers), 0)
+        self.assertEqual(len(self.u1.following), 1)
+
+        self.assertEqual(self.u2.followers[0].id, self.u1.id)
+        self.assertEqual(self.u1.following[0].id, self.u2.id)
+
+    def test_is_following(self):
+        self.u1.following.append(self.u2)
+        db.session.commit()
+
+        # User 1 should be following user 2
+        self.assertTrue(self.u1.is_following(self.u2))
+        self.assertFalse(self.u2.is_following(self.u1))
+        
+
+    def test_is_followed_by(self):
+        self.u1.following.append(self.u2)
+        db.session.commit()
+
+        # Similar to test_is_following but using is_followed_by
+        self.assertTrue(self.u2.is_followed_by(self.u1))
+        self.assertFalse(self.u1.is_followed_by(self.u2))
+
+    def test_signup(self):
+        user = User.signup("user3", "testemail3@gmail.com", "abc123", None)
+        user.id = 3
+        db.session.commit()
+
+        user = User.query.get(3)
+    
+        self.assertEqual(user.username, "user3")
+        self.assertEqual(user.email, "testemail3@gmail.com")
+
+        # Should not be equal as password should be encrypted
+        self.assertNotEqual(user.password, "abc123")
+
+    def test_authentication(self):
+        u = User.authenticate(self.u1.username, "abc123")
+        self.assertEqual(u.id, self.u1.id)
+    
+    def test_wrong_username(self):
+        self.assertFalse(User.authenticate("wrong", "abc123"))
+
+    def test_wrong_password(self):
+        self.assertFalse(User.authenticate(self.u1.username, "wrong"))
+
